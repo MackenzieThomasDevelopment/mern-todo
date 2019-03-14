7a6d5a43545bcc10f12618725877b427cd3615d7
Mackenzie Thomas <mackenzie.thomas54@gmail.com>
2019-03-14 16:51:34 -0500

    added kitsu api and started login page

--- src/App.js
+++ src/App.js
@@ -6,10 +6,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
+import Login from "./components/login.component";
 
 import logo from "./logo.svg";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://kitsu.io/api/edge/anime")
+      .then(Response => Response.json())
+      .then(findResponse => {
+        console.log(findResponse.data);
+        this.setState({
+          data: findResponse.data
+        });
+      });
+  }
+
   render() {
     return (
       <Router>
@@ -43,6 +62,11 @@ class App extends Component {
                     Create Todo
                   </Link>
                 </li>
+                <li className="navbar-item">
+                  <Link to="/login" className="nav-link">
+                    Login
+                  </Link>
+                </li>
               </ul>
             </div>
           </nav>
@@ -50,6 +74,7 @@ class App extends Component {
           <Route path="/" exact component={TodosList} />
           <Route path="/edit/:id" component={EditTodo} />
           <Route path="/create" component={CreateTodo} />
+          <Route path="/login" component={Login} />
         </div>
       </Router>
     );
@@ -57,3 +82,51 @@ class App extends Component {
 }
 
 export default App;
+
+// <Router>
+//   <div className="container">
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//       <a
+//         className="navbar-brand"
+//         href="https://codingthesmartway.com"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         <img
+//           src={logo}
+//           width="30"
+//           height="30"
+//           alt="CodingTheSmartWay.com"
+//         />
+//       </a>
+//       <Link to="/" className="navbar-brand">
+//         MERN-Stack Todo App
+//       </Link>
+//       <div className="collpase navbar-collpase">
+//         <ul className="navbar-nav mr-auto">
+//           <li className="navbar-item">
+//             <Link to="/" className="nav-link">
+//               Todos
+//             </Link>
+//           </li>
+//           <li className="navbar-item">
+//             <Link to="/create" className="nav-link">
+//               Create Todo
+//             </Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//     <br />
+//     <Route path="/" exact component={TodosList} />
+//     <Route path="/edit/:id" component={EditTodo} />
+//     <Route path="/create" component={CreateTodo} />
+//   </div>
+// </Router>
+/* <div>
+          {this.state.data.map((dynamicData, key) => (
+            <div>
+              <img src={dynamicData.attributes.posterImage.large} />
+            </div>
+          ))}
+        </div> */
